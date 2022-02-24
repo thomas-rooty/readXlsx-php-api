2ebb8016acf98b5afac64a421d6167e948c96a68
thomas-rooty <essainono1@gmail.com>
2022-02-24 10:25:10 +0100

    PHP API to read cells with full excel calculation engine

--- readXlsx.php
+++ readXlsx.php
@@ -1,5 +1,10 @@
 <?php
 
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
 if (isset($_FILES['file']['name'])) {
     // file name
     $filename = $_FILES['file']['name'];
@@ -9,13 +14,23 @@ if (isset($_FILES['file']['name'])) {
 
     // Check if file is a xlsx file
     $fileType = pathinfo($location, PATHINFO_EXTENSION);
-
     $response = 0;
 
     if ($fileType === 'xlsx') {
         // Upload file
         if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
-            $response = 'File uploaded successfully';
+            // Get this file
+            $spreadsheet = IOFactory::load($location);
+            // Create contractPrices array and get cells from cellsToRead array on sheet 2
+            $prix = array(
+                "ivoire" => $spreadsheet->getSheet(2)->getCell('D238')->getCalculatedValue(),  //[0]
+                "silver" => $spreadsheet->getSheet(2)->getCell('D239')->getCalculatedValue(),  //[1]
+                "gold" => $spreadsheet->getSheet(2)->getCell('D241')->getCalculatedValue(),    //[2]
+                "goldp" => $spreadsheet->getSheet(2)->getCell('D243')->getCalculatedValue(),    //[2]
+                "platinium" => $spreadsheet->getSheet(2)->getCell('D245')->getCalculatedValue() //[4]
+            );
+            // Return response with all the prix
+            $response = 'Ivoire : ' . number_format($prix['ivoire'], 1) . ', Silver : ' . number_format($prix['silver'], 1) . ', Gold : ' . number_format($prix['gold'], 1) . ', Gold+ : ' . number_format($prix['goldp'], 1) . ', Platinium : ' . number_format($prix['platinium'], 1);
         } else {
             $response = 'File upload failed, please try again.';
         }
@@ -27,6 +42,6 @@ if (isset($_FILES['file']['name'])) {
     echo $response;
 
     // Delete the file and exit the script
-    unlink($location);
+    //unlink($location);
     exit;
 }
